dff8f2cc0168460b19cf048789758bb168285364
Andrey <brovkin_58@mail.ru>
2023-06-05 12:44:21 +0300

    Introduced

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,21 @@
-// eslint-disable-next-line no-undef
-document.getElementById('header').innerHTML = 'Welcome to the Brain Games!';
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-alert */
+/* eslint-disable no-undef */
+const greeting = 'Welcome to the Brain Games!';
+alert(greeting);
+document.getElementById('header').innerHTML = greeting;
+
+const askName = 'May I have your name?';
+const namePlayer = prompt(askName);
+const greetingWithName = `Hello, ${namePlayer}`;
+alert(greetingWithName);
+document.getElementById('name').innerHTML = greetingWithName;
+
+// const play = confirm('Do you want to play?');
+const play = confirm(`${namePlayer}, do you want to play?`);
+
+if (play) {
+  // something code
+} else {
+  alert('That so sad (;');
+}
