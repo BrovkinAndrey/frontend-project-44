c0a26e3a6d2fe32206367e44fb94b530cd4e4fca
Andrey <brovkin_58@mail.ru>
2023-06-09 15:33:55 +0300

    add new game "brain-prime"

---
+++ bin/brain-prime.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-console */
+import PromptSync from 'prompt-sync';
+import game from './brain-games.js';
+
+const prompt = PromptSync({ sigint: true });
+const primeNumbers = [2, 3, 5, 7, 11];
+
+const isPrime = (num) => !primeNumbers.some((prime) => num > prime && num % prime === 0);
+
+function round() {
+  const num = Math.floor(Math.random() * 20) + 2;
+  const answer = isPrime(num) ? 'yes' : 'no';
+  console.log(`Question: ${num}`);
+  const userAnswer = prompt('Your asnwer: ');
+  return answer === userAnswer;
+}
+
+game("Answer 'yes' if given number is prime. Otherwise answer 'no'.", round);
