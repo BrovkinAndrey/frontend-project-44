9d527c907a1aeab3f53d6afa6dfb35589c4a51e2
Andrey <brovkin_58@mail.ru>
2023-06-09 13:56:45 +0300

    add game is-even

---
+++ bin/brain-even.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+import PromptSync from 'prompt-sync';
+import game from './brain-games.js';
+
+const prompt = PromptSync({ sigint: true });
+
+const isEven = (num) => num % 2 === 0;
+
+function round() {
+  const num = Math.floor(Math.random() * 100) + 1;
+  const answer = isEven(num) ? 'yes' : 'no';
+  console.log(`Question: ${num}`);
+  const userAnswer = prompt('Your asnwer: ');
+  return answer === userAnswer;
+}
+
+game("Answer 'yes' if number even otherwise answer 'no'.", round);
